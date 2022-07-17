dc1f8d54a531b9c743e51c36494635fcb3ab33ca
Aryan384 <58809585+Aryan384@users.noreply.github.com>
2022-07-17 15:17:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('bot online yay boy!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+require("dotenv").config();
 const Discord = require("discord.js")
 const request = require('request')
 const rcon = require("./rcon/app.js")
 const SourceQuery = require('sourcequery')
 const fs = require('fs');
+const RPC = require('discord-rpc');
+const embed = new Discord.MessageEmbed();
+
+const rpc = new RPC.Client({
+  transport: 'ipc'
+});
 
 const configdir = './config';
 const maxServers = 10;
+
+
+
+ 
+   
+  
 
 // Create dir if not exist
 if (!fs.existsSync(configdir)){
@@ -42,85 +64,69 @@ fs.readdir(configdir, (err, files) => {
 
         // Functions
         function updateActivity() {
-            if (apiSite == 1) {
-                require("tls").DEFAULT_ECDH_CURVE = "auto"
-                request({ url: apiUrl, headers: { json: true, Referer: 'discord-rustserverstatus' }, timeout: 10000 }, function (err, res, body) {
-                    if (!err && res.statusCode == 200) {
-                        const server = JSON.parse(body)
-                        const is_online = server.status
-                        if (is_online == "Online") {
-                            const players = server.players
-                            const maxplayers = server.players_max
-                            if (debug) console.log("Updated rust-servers.info")
-                            status = `${players}/${maxplayers}`
-                            return client.user.setActivity(status, { type: statusType })
-                        } else {
-                            return client.user.setActivity("Offline")
-                        }
-                    }
-                })
-            }
-            if (apiSite == 2) {
-                request({ url: apiUrl, headers: { Referer: 'discord-rustserverstatus' }, timeout: 10000 }, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        const server = JSON.parse(body)
-                        const is_online = server.is_online
-                        if (is_online == 1) {
-                            const players = server.players
-                            const maxplayers = server.maxplayers
-                            if (debug) console.log("Updated rust-servers.net")
-                            let status = `${players}/${maxplayers}`
-                            return client.user.setActivity(status, { type: statusType })
-                        } else {
-                            return client.user.setActivity("Offline")
-                        }
-                    }
-                })
-            }
             if (apiSite == 3) {
                 require("tls").DEFAULT_ECDH_CURVE = "auto"
-                request({ url: apiUrl, headers: { json: true, Referer: 'discord-rustserverstatus' }, timeout: 10000 }, function (err, res, body) {
+                request({ url: apiUrl, headers: { json: true, Referer: 'discord-squadserverstatus' }, timeout: 10000 }, function (err, res, body) {
                     if (!err && res.statusCode == 200) {
+                        
                         const jsonData = JSON.parse(body)
                         const server = jsonData.data.attributes
+                        const mapserver = jsonData.data.attributes.details;
+                        const nameserver = jsonData.data.attributes;
+                        const queueserver = jsonData.data.attributes;
                         const is_online = server.status
+                        
                         if (is_online == "online") {
                             const players = server.players
+                            const name = nameserver.name;
                             const maxplayers = server.maxPlayers
-                            const queue = server.details.rust_queued_players
-                            let status = `${players}/${maxplayers}`
+                            const queue = queueserver.queue;
+                            const map = mapserver.map;
+
+                            
+
+
+
+
+                            
+                            
+                       
+
+
+
+
+
+
+                           let status = `${players}/${maxplayers}` + 
+                           ` Map : ${map}`
+
+                           
+
+                           //if(players > 50){
+                                //status += `(${seedingmessage})`
+                            //} else if (players < 50){
+                            //   status += `\n(Server is SEEDING)`
+                           // }
+                            if(players > 100){
+                              status += `(${queue} ${queueMessage})`
+                            } 
                             if (typeof queue !== "undefined" && queue != "0") {
-                                status += ` (${queue} ${queueMessage})`
+                              status += ` (${queue} ${queueMessage})`
                             }
-                            if (debug) console.log("Updated from battlemetrics, serverid: " + server.id)
-                            return client.user.setActivity(status, { type: statusType })
-                        } else {
+                           if (debug) console.log("Updated from battlemetrics, serverid: " + server.id)
+                            return client.user.setActivity(status, {type: statusType})
+                                 
+                        }
+                        
+                        else {
                             return client.user.setActivity("Offline")
                         }
                     }
+                
                 })
             }
-            if (apiSite == 4) {
-                if (!serverIp || !serverPort) {
-                    console.log("You have to configure serverIP/port")
-                    process.exit()
-                } else {
-                    const sq = new SourceQuery(1000) // 1000ms timeout
-                    sq.open(serverIp, serverPort)
-
-                    sq.getInfo(function(err, info) {
-                        if (err) { return client.user.setActivity("Offline") }
-                        else {
-                            if (debug) { console.log('Server Info: \nIP: %s\nPort: %s\nName: %s\nPlayers: %s/%s', serverIp, serverPort, info.name, info.players, info.maxplayers) }
-                            const players = info.players
-                            const maxplayers = info.maxplayers
-                            let status = `${players}/${maxplayers}`
-                            return client.user.setActivity(status, { type: statusType })
-                        }
-                    })
-                }
-            }
         }
+    
         // End Functions
 
         try {
@@ -130,7 +136,7 @@ fs.readdir(configdir, (err, files) => {
         }
         const client = new Discord.Client()
 
-        const updateInterval = (1000 * 60) * 3 || (1000 * 60) * process.env.updateInterval || (1000 * 60) * config.updateInterval
+        const updateInterval = (15 * 1000) * 1 || (15 * 1000) * process.env.updateInterval || (1000 * 60) * config.updateInterval
         const debug = process.env.debug || config.debug
         const apiUrl = process.env.apiUrl || config.apiUrl
         const apiSite = process.env.apiSite || config.apiSite
@@ -140,6 +146,7 @@ fs.readdir(configdir, (err, files) => {
         const prefix = process.env.prefix || config.prefix
         const roles = process.env.roles || config.roles
         const queueMessage = process.env.queueMessage || config.queueMessage
+        const seedingmessage = process.env.seedingmessage || config.seedingmessage
         const statusType = process.env.statusType || config.statusType
 
         client.on("ready", () => {
@@ -203,6 +210,6 @@ fs.readdir(configdir, (err, files) => {
             return console.error('Unhandled promise rejection:', error);
         });
         
-        client.login(process.env.token || config.token)
+        client.login(process.env.TOKEN)
     }
 });    
